dd7c52ec4291c86bec389f5588bd93670f39e721
Rizwaan Salim <argenspin@gmail.com>
2024-10-05 13:20:27 +0530

    Added a test endpoint

--- voxbay_connector/controllers/voxbay.py
+++ voxbay_connector/controllers/voxbay.py
@@ -1,8 +1,21 @@
 from odoo import http
 from odoo.http import request
 import logging
-
+import json
 class VoxbayApi(http.Controller):
-    @http.route(['/voxbay/api/incoming_landed'], type='http', auth="public", website=True)
-    def courses_page(self,**kw):
-        print(kw)
+    # Event 1: Incoming call landed on server
+    @http.route(
+        '/voxbay/api/incoming_landed',
+        type='json', auth='none', methods=["POST"], csrf=False)
+    def authenticate(self, *args, **post):
+        print(post.items())
+
+        # Prepare the response data
+        response_data = {
+            'status': 'success',
+            'message': 'Incoming call data received',
+            'received_data': post  # Echo the received data back to the client
+        }
+        
+        # Return the response in JSON format
+        return json.dumps(response_data)
