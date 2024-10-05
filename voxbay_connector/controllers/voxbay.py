a5e9c66a2434e27148f68e71a99f90ef26454466
Rizwaan Salim <argenspin@gmail.com>
2024-10-05 15:11:47 +0530

    add some more endpoints to voxbay api

--- voxbay_connector/controllers/voxbay.py
+++ voxbay_connector/controllers/voxbay.py
@@ -4,6 +4,10 @@ import logging
 import json
 _logger = logging.getLogger("Voxbay Debug: ")
 class VoxbayApi(http.Controller):
+
+    '''
+        *****Incoming Call API******
+    '''
     # Event 1: Incoming call landed on server
     @http.route(
         '/voxbay/api/incoming_landed',
@@ -14,9 +18,92 @@ class VoxbayApi(http.Controller):
         # Prepare the response data
         response_data = {
             'status': 'success',
-            'message': 'Incoming call data received',
+            'message': 'incoming_landed data received',
             'received_data': post  # Echo the received data back to the client
         }
-        
+        # Return the response in JSON format
+        return json.dumps(response_data)
+    
+    #Event 2: Call answered by an agent
+    @http.route(
+        '/voxbay/api/incoming_answered',
+        type='json', auth='none', methods=["POST"], csrf=False)
+    def authenticate(self, *args, **post):
+        _logger.info(post)
+
+        # Prepare the response data
+        response_data = {
+            'status': 'success',
+            'message': 'incoming_answered data received',
+            'received_data': post  # Echo the received data back to the client
+        }
+        # Return the response in JSON format
+        return json.dumps(response_data)
+    
+    #Event 3: When a call is disconnected
+    @http.route(
+        '/voxbay/api/incoming_disconnected',
+        type='json', auth='none', methods=["POST"], csrf=False)
+    def authenticate(self, *args, **post):
+        _logger.info(post)
+
+        # Prepare the response data
+        response_data = {
+            'status': 'success',
+            'message': 'incoming_disconnected data received',
+            'received_data': post  # Echo the received data back to the client
+        }
+        # Return the response in JSON format
+        return json.dumps(response_data)
+    
+    # Event 4: CDR push at the end of the call
+    @http.route(
+        '/voxbay/api/incoming_cdr_push',
+        type='json', auth='none', methods=["POST"], csrf=False)
+    def authenticate(self, *args, **post):
+        _logger.info(post)
+
+        # Prepare the response data
+        response_data = {
+            'status': 'success',
+            'message': 'incoming_cdr_push data received',
+            'received_data': post  # Echo the received data back to the client
+        }
+        # Return the response in JSON format
+        return json.dumps(response_data)
+    
+    '''
+        *****Outgoing Call API******
+    '''
+
+    # Event 1: when an outgoing call initiated
+    @http.route(
+        '/voxbay/api/outgoing_initiated',
+        type='json', auth='none', methods=["POST"], csrf=False)
+    def authenticate(self, *args, **post):
+        _logger.info(post)
+
+        # Prepare the response data
+        response_data = {
+            'status': 'success',
+            'message': 'incoming_cdr_push data received',
+            'received_data': post  # Echo the received data back to the client
+        }
+        # Return the response in JSON format
+        return json.dumps(response_data)
+    
+    # Event 2: CDR push at the end of the call
+    @http.route(
+        '/voxbay/api/outgoing_cdr_push',
+        type='json', auth='none', methods=["POST"], csrf=False)
+    def authenticate(self, *args, **post):
+        _logger.info(post)
+
+        # Prepare the response data
+        response_data = {
+            'status': 'success',
+            'message': 'outgoing_cdr_push data received',
+            'received_data': post  # Echo the received data back to the client
+        }
         # Return the response in JSON format
         return json.dumps(response_data)
