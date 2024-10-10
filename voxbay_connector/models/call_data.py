d47b2bf265a0e8d962657e86168b1d2ba4740fee
Rizwaan Salim <argenspin@gmail.com>
2024-10-10 21:53:49 +0530

    Label changes

--- voxbay_connector/models/call_data.py
+++ voxbay_connector/models/call_data.py
@@ -15,7 +15,7 @@ class VoxbayCallData(models.Model):
     call_uuid = fields.Char(string="Call UUID")
     call_type = fields.Selection(string="Call Type", selection=[('incoming', 'Incoming Call'), ('outgoing', 'Outgoing Call')])
     event_status = fields.Selection(string="Event Status", selection=[('agent_received_call', 'Agent Received Call'), ('agent_answered_call', 'Agent Accepted Call'), ('agent_initiated_call', 'Agent Initiated Call'), ('call_ended', 'Call Ended')])
-    total_call_duration = fields.Char(string="Call Duration", readonly=True)
+    total_call_duration = fields.Char(string="Duration", readonly=True)
     call_date = fields.Datetime(string="Call Date")
     call_status = fields.Selection(string="Call Status", selection=[('Connected', 'Connected'), ('Not Connected', 'Not Connected'), ('Cancel', 'Cancel'), ('NOANSWER', 'No Answer'), ('BUSY', 'BUSY'), ('CONGESTION', 'Congestion'), ('CHANUNAVAIL', 'Channel Unavailable'), ('FAILED', 'Failed')])
 
@@ -26,8 +26,8 @@ class VoxbayCallData(models.Model):
 
     recording_url = fields.Char(string="Recording")
     
-    call_start_time = fields.Datetime(string="Call Start Time")
-    call_end_time = fields.Datetime(string="Call End Time")
+    call_start_time = fields.Datetime(string="Start Time")
+    call_end_time = fields.Datetime(string="End Time")
     dtmf = fields.Char(string="DTMF Sequence")
     transferred_number = fields.Char("Transferred Number")
 
@@ -35,5 +35,5 @@ class VoxbayCallData(models.Model):
     extension_number = fields.Char(string="Extension Number") #Not present in data received by voxbay api
     destination_number = fields.Char(string="Destination Number") #Not present in data received by voxbay api
     caller_id = fields.Char(string="Caller ID") #Not present in data received by voxbay api
-    conversation_duration = fields.Char("Call Conversation Duration")
+    conversation_duration = fields.Char("Conversation Duration")
 
