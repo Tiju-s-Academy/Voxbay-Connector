f744e8e3a872cf79f35bbc1f1401498099dd8fcf
Rizwaan Salim <argenspin@gmail.com>
2024-10-10 22:15:27 +0530

    convert duration to int field. some other view changes

--- voxbay_connector/models/call_data.py
+++ voxbay_connector/models/call_data.py
@@ -15,11 +15,10 @@ class VoxbayCallData(models.Model):
     call_uuid = fields.Char(string="Call UUID")
     call_type = fields.Selection(string="Call Type", selection=[('incoming', 'Incoming Call'), ('outgoing', 'Outgoing Call')])
     event_status = fields.Selection(string="Event Status", selection=[('agent_received_call', 'Agent Received Call'), ('agent_answered_call', 'Agent Accepted Call'), ('agent_initiated_call', 'Agent Initiated Call'), ('call_ended', 'Call Ended')])
-    total_call_duration = fields.Char(string="Duration", readonly=True)
+    total_call_duration = fields.Integer(string="Duration",)
     call_date = fields.Datetime(string="Call Date")
     call_status = fields.Selection(string="Call Status", selection=[('Connected', 'Connected'), ('Not Connected', 'Not Connected'), ('Cancel', 'Cancel'), ('NOANSWER', 'No Answer'), ('BUSY', 'BUSY'), ('CONGESTION', 'Congestion'), ('CHANUNAVAIL', 'Channel Unavailable'), ('FAILED', 'Failed')])
 
-    # Incoming
     called_number = fields.Char(string="Called Number") # Customer Number (Outgoing Calls), Operator Number (Incoming Calls)
     caller_number = fields.Char(string="Caller Number") # Customer Number (Incoming Calls), Operator Number (Outgoing Calls)
     agent_number = fields.Char(string="Number of Agent") #AgentNumber
@@ -31,9 +30,8 @@ class VoxbayCallData(models.Model):
     dtmf = fields.Char(string="DTMF Sequence")
     transferred_number = fields.Char("Transferred Number")
 
-    # Outgoing
     extension_number = fields.Char(string="Extension Number") #Not present in data received by voxbay api
     destination_number = fields.Char(string="Destination Number") #Not present in data received by voxbay api
     caller_id = fields.Char(string="Caller ID") #Not present in data received by voxbay api
-    conversation_duration = fields.Char("Conversation Duration")
+    conversation_duration = fields.Integer("Conversation Duration")
 
