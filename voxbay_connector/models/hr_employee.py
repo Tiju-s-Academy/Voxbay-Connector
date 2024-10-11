508de4b8fcbec2c680b9347857ec2e5f13449c71
Rizwaan Salim <argenspin@gmail.com>
2024-10-11 17:52:49 +0530

    Added smart buttons to go to employee call records

--- voxbay_connector/models/hr_employee.py
+++ voxbay_connector/models/hr_employee.py
@@ -1,10 +1,25 @@
-from odoo import models, fields, api
+from odoo import models, fields, api, _
 from odoo.exceptions import ValidationError
-
+import logging
+_logger = logging.getLogger("Voxbay Debug:")
 class HrEmployee(models.Model):
     _inherit = "hr.employee"
 
     agent_number = fields.Char(string="Voxbay Agent Number")
+
+    voxbay_incoming_calls = fields.One2many('voxbay.call.data.record', 'operator_employee_id', string="Incoming Calls", domain=[('call_type','=','incoming')])
+    voxbay_incoming_calls_count = fields.Integer(compute="_compute_voxbay_incoming_calls_count")
+    @api.depends('voxbay_incoming_calls')
+    def _compute_voxbay_incoming_calls_count(self):
+        for record in self:
+            record.voxbay_incoming_calls_count = len(record.voxbay_incoming_calls)
+
+    voxbay_outgoing_calls = fields.One2many('voxbay.call.data.record', 'operator_employee_id', string="Outgoing Calls", domain=[('call_type','=','outgoing')])
+    voxbay_outgoing_calls_count = fields.Integer(compute="_compute_voxbay_outgoing_calls_count")
+    @api.depends('voxbay_outgoing_calls')
+    def _compute_voxbay_outgoing_calls_count(self):
+        for record in self:
+            record.voxbay_outgoing_calls_count = len(record.voxbay_outgoing_calls)
 
     @api.onchange('agent_number')
     def onchange_agent_number(self):
@@ -15,3 +30,23 @@ class HrEmployee(models.Model):
             employee_with_same_agent_no = self.env['hr.employee'].sudo().search(domain, limit=1)
             if employee_with_same_agent_no:
                 raise ValidationError(f"This Agent Number is already assigned to {employee_with_same_agent_no[0].name}! You need to unassign this Agent Number from {employee_with_same_agent_no[0].name}, before setting it to another Employee.")
+            
+    def action_view_incoming_calls(self):
+        return {
+            'name': _("Incoming Calls"),
+            'type': 'ir.actions.act_window',
+            'res_model': 'voxbay.call.data.record',
+            'context': {'create': False},
+            'view_mode': 'list',
+            'domain': [('id', 'in', self.voxbay_incoming_calls.ids)],
+        }
+    
+    def action_view_outgoing_calls(self):
+        return {
+            'name': _("Outgoing Calls"),
+            'type': 'ir.actions.act_window',
+            'res_model': 'voxbay.call.data.record',
+            'context': {'create': False},
+            'view_mode': 'list',
+            'domain': [('id', 'in', self.voxbay_outgoing_calls.ids)],
+        }
