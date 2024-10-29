df0898796d0ff0561c7724c513d26246c40bf820
Rizwaan Salim <argenspin@gmail.com>
2024-10-29 17:53:33 +0530

    log message

--- telinfy_connector/controllers/telinfy.py
+++ telinfy_connector/controllers/telinfy.py
@@ -12,5 +12,8 @@ class TelinfyApi(http.Controller):
     @http.route('/telinfy/whatsapp/webhook', type='json', auth='none', methods=["POST"], csrf=False)
     def incoming_landed(self, *args, **post):
         post_data: dict = request.httprequest.json
+        headers = dict(request.httprequest.headers)
+        # _logger.error(f'Request: {request.httprequest.headers.items()}')
         _logger.error(f'Webhook Data: {post_data}')
+        _logger.error(f'Headers: {headers}')
         return json.dumps({'status': 'success',})
