e0e8fed621d49dfd470b2d05c9c8f13dc9946650
Rizwaan Salim <argenspin@gmail.com>
2024-10-30 12:59:58 +0530

    Create lead from telinfy webhook

--- telinfy_connector/controllers/telinfy.py
+++ telinfy_connector/controllers/telinfy.py
@@ -1,4 +1,4 @@
-from odoo import http
+from odoo import http, SUPERUSER_ID
 from odoo.http import request
 import logging
 import json
@@ -12,8 +12,25 @@ class TelinfyApi(http.Controller):
     @http.route('/telinfy/whatsapp/webhook', type='json', auth='none', methods=["POST"], csrf=False)
     def incoming_landed(self, *args, **post):
         post_data: dict = request.httprequest.json
-        headers = dict(request.httprequest.headers)
-        # _logger.error(f'Request: {request.httprequest.headers.items()}')
         _logger.error(f'Webhook Data: {post_data}')
-        _logger.error(f'Headers: {headers}')
+        if post_data.get('messages'):
+            messages = post_data.get('messages')
+            for message in messages:
+                if message.get('type','')=='text':
+                    superuser = request.env['res.users'].sudo().browse(SUPERUSER_ID)
+                    from_number = message['from']
+                    lead_name = post_data.get('contacts')[0]['profile']['name']
+                    lead = request.env['crm.lead'].sudo().search([('phone','like',from_number), ('phone','!=',False)], limit=1)
+                    if lead:
+                        _logger.info(f'Lead already exists for this whatsapp contact {lead_name}, {from_number}.')
+                    else:
+                        lead = request.env['crm.lead'].with_user(superuser).create({
+                        'name': f'[Whatsapp] {lead_name}',
+                        'partner_id': request.env['res.partner'].sudo().create({'name': lead_name, 'company_type': 'person', 'phone': from_number}).id,
+                        'phone': from_number,
+                        'user_id': superuser.id,
+                        'description': message['text']['body'],
+                        'type': 'lead',  
+                        })
+                        _logger.info(f'Lead {lead_name}, {from_number} created successfully!')
         return json.dumps({'status': 'success',})
