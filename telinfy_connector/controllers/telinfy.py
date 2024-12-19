fd0e4a07fe17de35b1fc3ea6d3c95b6d757856a8
Muhammed Anas <muhammadanaswayanad@gmail.com>
2024-12-19 15:13:03 +0530

    Update telinfy.py

--- telinfy_connector/controllers/telinfy.py
+++ telinfy_connector/controllers/telinfy.py
@@ -12,7 +12,7 @@ class TelinfyApi(http.Controller):
     # Event 1: Incoming call landed on server
     @http.route('/telinfy/whatsapp/webhook', type='json', auth='none', methods=["POST"], csrf=False)
     def incoming_landed(self, *args, **post):
-        post_data: dict = request.jsonrequest
+        post_data: dict = request.httprequest.json
         _logger.error(f'Webhook Data: {post_data}')
         if post_data.get('messages'):
             messages = post_data.get('messages')
