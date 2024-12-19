e909d26b33248f4d5b5eb512b017dcb30d8e2918
Muhammed Anas <muhammadanaswayanad@gmail.com>
2024-12-19 15:00:46 +0530

    Update telinfy.py

--- telinfy_connector/controllers/telinfy.py
+++ telinfy_connector/controllers/telinfy.py
@@ -3,7 +3,8 @@ from odoo.http import request
 import logging
 import json
 import traceback
-_logger = logging.getLogger("Telinfy Debug: ")
+_logger = logging.getLogger("TelinfyDebug")
+SOURCE_ID_WHATSAPP = 81
 import random
 
 class TelinfyApi(http.Controller):
@@ -11,7 +12,7 @@ class TelinfyApi(http.Controller):
     # Event 1: Incoming call landed on server
     @http.route('/telinfy/whatsapp/webhook', type='json', auth='none', methods=["POST"], csrf=False)
     def incoming_landed(self, *args, **post):
-        post_data: dict = request.httprequest.json
+        post_data: dict = request.jsonrequest
         _logger.error(f'Webhook Data: {post_data}')
         if post_data.get('messages'):
             messages = post_data.get('messages')
@@ -23,8 +24,6 @@ class TelinfyApi(http.Controller):
                     lead = request.env['crm.lead'].sudo().search([('phone','like',from_number), ('phone','!=',False)], limit=1)
                     if lead:
                         _logger.info(f'Lead already exists for this whatsapp contact {lead_name}, {from_number}.')
-                        # Add message to chatter
-                        lead.message_post(body=f"WhatsApp Message: {message['text']['body']}")
                     else:
                         sales_team = False
                         sales_teams = request.env['crm.team'].sudo().search([])
@@ -40,10 +39,13 @@ class TelinfyApi(http.Controller):
                         'team_id': sales_team.id,
                         'description': f"<p>{message['text']['body']}</p>",
                         'type': 'lead',
-                        'source_id': 81,
+                        'source_id': SOURCE_ID_WHATSAPP,
                         })
                         _logger.info(f'Lead {lead_name}, {from_number} created successfully!')
+                    # Add message to chatter
                 else:
                     # Add message to chatter for non-text messages
+                    if lead:
+                        lead.message_post(body=f"WhatsApp Message: {message}")
                     lead.message_post(body=f"WhatsApp Message: {message}")
         return json.dumps({'status': 'success',})
